a0cf0fffe6fbfe970312c3c0abf2a9d45a41379b
Carlos Miguel Jenkins Perez <carlos.jenkins@hpe.com>
2016-03-11 19:05:31 -0600

    new: usr: Supporting prefix/suffix and exceptions..
    Change-Id: If2189050494adadcf89c0fc99de2e98dab19bdf5
    Signed-off-by: Carlos Jenkins <carlos.jenkins@hpe.com>

--- tools/vtysh_meta.py
+++ tools/vtysh_meta.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
@@ -17,22 +16,13 @@
 # under the License.
 
 """
-VTYSH specification definition and generation module.
+Vtysh meta-specification file.
 """
 
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 
-import logging
-from re import sub
 from collections import OrderedDict
-from os.path import abspath, normpath, dirname, join
-
-from inflection import parameterize, underscore, camelize
-from jinja2 import FunctionLoader, Environment, StrictUndefined
-
-
-log = logging.getLogger(__name__)
 
 
 VTYSH_SPEC = OrderedDict([
@@ -54,7 +44,7 @@ VTYSH_SPEC = OrderedDict([
                 'returns': True
             },
             {
-                'command': 'show vlan {vlanid}',
+                'command': 'show vlan',
                 'doc': 'Show VLAN configuration.',
                 'arguments': [
                     {
@@ -78,7 +68,7 @@ VTYSH_SPEC = OrderedDict([
                 'returns': True
             },
             {
-                'command': 'show lacp aggregates {lag}',
+                'command': 'show lacp aggregates',
                 'doc': 'Show LACP aggregates.',
                 'arguments': [
                     {
@@ -331,7 +321,7 @@ VTYSH_SPEC = OrderedDict([
                 'returns': True
             },
             {
-                'command': 'show interface loopback {loopback_int}',
+                'command': 'show interface loopback',
                 'doc': 'Show loopback interfaces on ops',
                 'arguments': [
                     {
@@ -461,7 +451,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'ip route {ipv4} {next_hop} {metric}',
+                'command': 'ip route {ipv4} {next_hop}',
                 'doc': 'Configure static routes',
                 'arguments': [
                     {
@@ -480,7 +470,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no ip route {ipv4} {next_hop} {metric}',
+                'command': 'no ip route {ipv4} {next_hop}',
                 'doc': 'Un-configure static routes',
                 'arguments': [
                     {
@@ -647,7 +637,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'ipv6 route {ipv6} {next_hop} {metric}',
+                'command': 'ipv6 route {ipv6} {next_hop}',
                 'doc': 'Configure static routes',
                 'arguments': [
                     {
@@ -666,7 +656,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no ipv6 route {ipv6} {next_hop} {metric}',
+                'command': 'no ipv6 route {ipv6} {next_hop}',
                 'doc': 'Un-configure static routes',
                 'arguments': [
                     {
@@ -746,7 +736,7 @@ VTYSH_SPEC = OrderedDict([
             },
             {
                 'command': (
-                    'sflow agent-interface {portlbl} {address_family}'
+                    'sflow agent-interface {portlbl}'
                 ),
                 'doc': 'Set sFlow agent interface',
                 'arguments': [
@@ -1015,7 +1005,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'logging {remote_host} {transport} {severity}',
+                'command': 'logging {remote_host}',
                 'doc': 'Configure Syslog Server',
                 'arguments': [
                     {
@@ -1047,7 +1037,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no logging {remote_host} {transport} {severity}',
+                'command': 'no logging {remote_host}',
                 'doc': 'Remove Syslog Server Configuration',
                 'arguments': [
                     {
@@ -1176,7 +1166,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no match ip address prefix-list {prefix_name}',
+                'command': 'no match ip address prefix-list',
                 'doc': 'Unset prefix-list',
                 'arguments': [
                     {
@@ -1197,7 +1187,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no set metric {metric}',
+                'command': 'no set metric',
                 'doc': 'Unset metric',
                 'arguments': [
                     {
@@ -1222,7 +1212,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no set community {community}',
+                'command': 'no set community',
                 'doc': 'Unset community',
                 'arguments': [
                     {
@@ -1992,8 +1982,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'nameserver {primary_nameserver} '
-                           '{secondary_nameserver}',
+                'command': 'nameserver {primary_nameserver}',
                 'doc': 'Configure the nameserver',
                 'arguments': [
                     {
@@ -2008,8 +1997,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no nameserver {primary_nameserver} '
-                           '{secondary_nameserver}',
+                'command': 'no nameserver {primary_nameserver}',
                 'doc': 'Configure the nameserver',
                 'arguments': [
                     {
@@ -2193,7 +2181,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no timers bgp {keepalive} {hold}',
+                'command': 'no timers bgp',
                 'doc': 'Sets the default values for keepalive interval and '
                        'hold time for a BGP router',
                 'arguments': [
@@ -2287,8 +2275,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'neighbor {peer} prefix-list {prefix_name} '
-                           '{filter_direction}',
+                'command': 'neighbor {peer} prefix-list {prefix_name}',
                 'doc': 'Applies a prefix-list to the neighbor to filter '
                        'updates to and from the neighbor',
                 'arguments': [
@@ -2309,8 +2296,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no neighbor {peer} prefix-list {prefix_name} '
-                           '{filter_direction}',
+                'command': 'no neighbor {peer} prefix-list {prefix_name}',
                 'doc': 'Remove a prefix-list filter from the neighbor',
                 'arguments': [
                     {
@@ -2345,7 +2331,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no neighbor {ip} description {text}',
+                'command': 'no neighbor {ip} description',
                 'doc': 'Removes a BGP neighbor',
                 'arguments': [
                     {
@@ -2414,7 +2400,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no neighbor {ip} timers {keepalive} {hold}',
+                'command': 'no neighbor {ip} timers',
                 'doc': 'Sets the default values for keepalive interval '
                        'and hold time for a specific BGP peer',
                 'arguments': [
@@ -2439,7 +2425,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'neighbor {ip} allowas-in {val}',
+                'command': 'neighbor {ip} allowas-in',
                 'doc': 'Specifies an allow-as-in occurrence number '
                        'for an AS to be in the AS path',
                 'arguments': [
@@ -2458,7 +2444,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no neighbor {ip} allowas-in {val}',
+                'command': 'no neighbor {ip} allowas-in',
                 'doc': 'Clears the allow-as-in occurrence number for '
                        'an AS to be in the AS path',
                 'arguments': [
@@ -2545,7 +2531,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'neighbor {ip_or_group} peer-group {group}',
+                'command': 'neighbor {ip_or_group} peer-group',
                 'doc': 'Assigns a neighbor to a peer-group',
                 'arguments': [
                     {
@@ -2567,7 +2553,7 @@ VTYSH_SPEC = OrderedDict([
                 ],
             },
             {
-                'command': 'no neighbor {ip_or_group} peer-group {group}',
+                'command': 'no neighbor {ip_or_group} peer-group',
                 'doc': 'Removes the neighbor from the peer-group',
                 'arguments': [
                     {
@@ -2660,265 +2646,23 @@ VTYSH_SPEC = OrderedDict([
     )
 ])
 
-"""VTYSH Specification as a Python dictionary"""
+"""Vtysh Specification as a Python dictionary"""
 
 
-VTYSH_TEMPLATE = """\
-{%- macro param_attrs(attrs) -%}
-{% if attrs -%}
-, {% for attr in attrs -%}
-{{ attr.name|variablize }}
-{%- if 'optional' in attr.keys() and attr.optional %}=''{% endif %}
-{%- if not loop.last %}, {% endif -%}
-{%- endfor %}
-{%- endif %}
-{%- endmacro -%}
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-# WARNING: This is auto-generated, do not manually modify
-
-\"""
-vtysh library.
-\"""
-
-from __future__ import unicode_literals, absolute_import
-from __future__ import print_function, division
-
-from .parser import *  # noqa
-
-
-class ContextManager(object):
-    \"""
-    This class defines a context manager object.
-
-    Usage:
-
-    ::
-
-        with ClassName(parameters) as ctx:
-            ctx.first_function()
-            ctx.second_function()
-
-    This way at the beginning the **pre_commands** will be run and at the end
-    the **post_commands** will clean the vtysh terminal. Every implementation
-    of this class document their pre_commands and post_commands.
-
-    \"""
-
-{% for context_name, context in spec.items() if context_name != 'root' %}
-class {{ context_name|objectize }}(ContextManager):
-    \"""
-    {{ context.doc|wordwrap(75)|indent(4) }}
-
-    pre_commands:
-
-    ::
-
-            {{ context.pre_commands }}
-
-    post_commands:
-
-    ::
-
-            {{ context.post_commands }}
-    \"""
-    def __init__({{ 'self, enode%s):'|format(param_attrs(context.arguments))|wordwrap(67)|indent(12) }}
-        self.enode = enode
-        {%- for arg in context.arguments %}
-        {% if arg.name == 'portlbl' -%}
-        self.port = enode.ports.get(portlbl, portlbl)
-        {%- else -%}
-        self.{{ arg.name }} = {{ arg.name }}
-        {%- endif %}
-        {%- endfor %}
-
-    def __enter__(self):
-        commands = \"""\\
-        {%- for pre_command in context.pre_commands %}
-            {{ pre_command }}
-        {%- endfor %}
-        \"""
-
-        self.enode.libs.common.assert_batch(
-            commands,
-            replace=self.__dict__,
-            shell='vtysh'
-        )
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        commands = \"""\\
-        {%- for post_command in context.post_commands %}
-            {{ post_command }}
-        {%- endfor %}
-        \"""
-
-        self.enode.libs.common.assert_batch(
-            commands,
-            replace=self.__dict__,
-            shell='vtysh'
-        )
-{% for command in context.commands %}
-    def {{ command.command|methodize }}(
-            {{ 'self%s):'|format(param_attrs(command.arguments))|wordwrap(56)|indent(12) }}
-        \"""
-        {{ command.doc|wordwrap(71)|indent(8) }}
-
-        This function runs the following vtysh command:
-
-        ::
-
-            # {{ command.command }}{% if command.command|length > 65%}  # noqa{% endif %}
-
-        {% for attr in command.arguments -%}
-        {{ ':param %s: %s'|format(attr.name, attr.doc)|wordwrap(70)|indent(12) }}
-        {% endfor -%}
-        {%- if 'returns' in command.keys() and command.returns -%}
-        :return: A dictionary as returned by
-         :func:`topology_lib_vtysh.parser.{{ 'parse_%s'|format(command.command|methodize) }}`
-        {% endif -%}
-        \"""{% if command.command|length > 66%}  # noqa{% endif %}
-        {%- for attr in command.arguments -%}
-            {% if attr.name == 'portlbl' %}
-        port = self.enode.ports.get(portlbl, portlbl)
-            {%- endif -%}
-        {%- endfor %}
-
-        cmd = (
-            '{{ command.command|wordwrap(64, wrapstring=" \'\n\'")|indent(12)}}'
-        )
-        result = self.enode(cmd.format(**locals()), shell='vtysh')
-
-        {% if 'returns' in command.keys() and command.returns -%}
-        {{ 'return parse_%s(result)'|format(command.command|methodize) }}
-        {%- else -%}
-        assert not result
-        {%- endif %}
-{% endfor %}
-{% endfor -%}
-
-{% for command in spec.root.commands %}
-def {{ command.command|methodize }}(
-        {{'enode%s):'|format(param_attrs(command.arguments))|wordwrap(67)|indent(8) }}
-    \"""
-    {{ command.doc|wordwrap(71)|indent(4) }}
-
-    This function runs the following vtysh command:
-
-    ::
-
-        # {{ command.command }}{% if command.command|length > 68%}  # noqa{% endif %}
-
-    {% for attr in command.arguments -%}
-    {{ ':param %s: %s'|format(attr.name, attr.doc)|wordwrap(75)|indent(5) }}
-    {% endfor -%}
-    {%- if 'returns' in command.keys() and command.returns -%}
-    :return: A dictionary as returned by
-     :func:`topology_lib_vtysh.parser.{{ 'parse_%s'|format(command.command|methodize) }}`
-    {% endif -%}
-    \"""{% if command.command|length > 69%}  # noqa{% endif %}
-    {%- for attr in command.arguments -%}
-        {% if attr.name == 'portlbl' %}
-    port = enode.ports.get(portlbl, portlbl)
-        {%- endif -%}
-    {%- endfor %}
-
-    cmd = (
-        '{{ command.command|wordwrap(67, wrapstring=" \'\n\'")|indent(8)}}'
+VTYSH_EXCEPTIONS_SPEC = OrderedDict([
+    (
+        'UnknownCommandException',
+        [
+            'Unknown command',
+        ]
+    ), (
+        'IncompleteCommandException',
+        [
+            'Command incomplete',
+        ]
     )
-    result = enode(cmd.format(**locals()), shell='vtysh')
-
-    {% if 'returns' in command.keys() and command.returns -%}
-    {{ 'return parse_%s(result)'|format(command.command|methodize) }}
-    {%- else -%}
-    assert not result
-    {%- endif %}
-
-{% endfor %}
-__all__ = [
-    'ContextManager',
-{%- for context_name in spec.keys() if context_name != 'root' %}
-    '{{ context_name|objectize }}',
-{%- endfor %}
-{%- for function in spec.root.commands %}
-    '{{ function.command|methodize }}'{% if not loop.last %},{% endif %}
-{%- endfor %}
-]
-
-"""  # noqa
+])
+"""Vtysh Exceptions specification as a Python dictionary"""
 
 
-def filter_objectize(token):
-    if token is None:
-        return None
-    return camelize(underscore(parameterize(underscore(token))))
-
-
-def filter_methodize(token):
-    if token is None:
-        return None
-    return underscore(parameterize(underscore(sub('{\S+}', '', token))))
-
-
-def filter_variablize(token):
-    if token is None:
-        return None
-    return underscore(parameterize(underscore(token)))
-
-
-def build():
-    """
-    Build VTYSH Python module from specification.
-    """
-
-    # Build template environment
-    def load_template(name):
-        templates = {
-            'vtysh': VTYSH_TEMPLATE
-        }
-        return templates[name]
-
-    env = Environment(
-        loader=FunctionLoader(load_template),
-        undefined=StrictUndefined
-    )
-    for ftr in ['objectize', 'methodize', 'variablize']:
-        env.filters[ftr] = globals()['filter_' + ftr]
-
-    # Render template
-    template = env.get_template('vtysh')
-    rendered = template.render(
-        spec=VTYSH_SPEC
-    )
-
-    # Write output
-    root = dirname(normpath(abspath(__file__)))
-
-    with open(join(
-            root,
-            '{}.py'.format('../lib/topology_lib_vtysh/library')),
-            'w') as module:
-        module.write(rendered)
-
-
-__all__ = ['VTYSH_SPEC']
-
-
-if __name__ == '__main__':
-    build()
+__all__ = ['VTYSH_SPEC', 'VTYSH_EXCEPTIONS_SPEC']
