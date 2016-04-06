a306284d69c95ec67d4e2768818f4e049784bd2d
Kattia Chaves <kchavesr@hpe.com>
2016-04-06 16:41:33 -0700

    dev: new: New CLI command exception for LAG not valid
    Change-Id: I615e75922eba26b09b88809bce14e287306b372e
    Signed-off-by: Kattia Chaves <kchavesr@hpe.com>

--- tools/vtysh_meta.py
+++ tools/vtysh_meta.py
@@ -2818,6 +2818,11 @@ VTYSH_EXCEPTIONS_SPEC = OrderedDict([
         [
             'Command incomplete',
         ]
+    ), (
+        'NotValidLAG',
+        [
+            'Specified LAG port doesn\'t exist.',
+        ]
     )
 ])
 """Vtysh Exceptions specification as a Python dictionary"""
