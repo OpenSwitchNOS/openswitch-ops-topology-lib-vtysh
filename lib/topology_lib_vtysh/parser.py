0c089391dc96230425c20f0ffa4d33bad09ad1e3
Mohanraju T <mohan.raj.thangamani@hpe.com>
2016-04-13 21:57:51 -0700

    Incorporated review comments
    Change-Id: If65fa5dcc7bb340fe32f1b4a4c6ff90f71c3ebe1
    Signed-off-by: Mohanraju T <mohan.raj.thangamani@hpe.com>

--- lib/topology_lib_vtysh/parser.py
+++ lib/topology_lib_vtysh/parser.py
@@ -3353,6 +3353,114 @@ def parse_show_mirror(raw_result):
         return result
 
 
+def parse_show_snmp_community(raw_result):
+
+    """
+    Parse the 'show snmp community' command raw output.
+
+    :param str raw_result: vtysh raw result string.
+    :rtype: dict
+    :return: The parsed result of the show snmp community\
+       command in a dictionary of the form
+
+    ::
+
+        {
+            '---------------------
+             SNMP communities
+             ---------------------'
+
+        }
+    """
+    community_string = []
+    pattern_found = 0
+    result = []
+    for line in raw_result.splitline():
+        if pattern_found == 2:
+            community_string.append(line)
+        else:
+            res = re.match(r'\s*-+\s*', line)
+            if res:
+                pattern_found = pattern_found + 1
+                res = None
+    result['community_string'] = res
+    if result == {}:
+        return None
+    else:
+        return result
+
+
+def parse_show_snmp_system(raw_result):
+
+    """
+    Parse the 'show snmp system' command raw output.
+
+    :param str raw_result: vtysh raw result string.
+    :rtype: dict
+    :return: The parsed result of the show snmp system\
+       command in a dictionary of the form
+
+    ::
+
+        {
+            'SNMP system information
+             ---------------------'
+
+        }
+    """
+    snmp_system_re = (
+        r'\s*SNMP system information\s*'
+        r'\s*-*\s*'
+        r'\s*System\description\s\:\s*(?P<system-description>.+)'
+        r'\s*System\location\s\:\s*(?P<system-location>.+)'
+        r'\s*System\contact\s\:\s*(?P<system-contact>.+)'
+        )
+
+    re_result = re.match(snmp_system_re, raw_result)
+    assert re_result
+
+    result = re_result.groupdict()
+
+    return result
+
+
+def parse_show_trap(raw_result):
+
+    """
+    Parse the 'show snmp trap' command raw output.
+
+    :param str raw_result: vtysh raw result string.
+    :rtype: dict
+    :return: The parsed result of the show snmp trap\
+       command in a dictionary of the form
+
+    ::
+
+        {
+            'show snmp trap
+            ------------------------------------------------------------------
+            Host              Port        Type           Version        SecName
+            ------------------------------------------------------------------'
+        }
+    """
+    host_ip_address = []
+    pattern_found = 0
+    result = []
+    for line in raw_result.splitline():
+        if pattern_found == 2:
+            host_ip_address.append(line)
+        else:
+            res = re.match(r'\s*-+\s*', line)
+            if res:
+                pattern_found = pattern_found + 1
+                res = None
+    result['host_ip_address'] = res
+    if result == {}:
+        return None
+    else:
+        return result
+
+
 __all__ = [
     'parse_show_vlan', 'parse_show_lacp_aggregates',
     'parse_show_lacp_interface', 'parse_show_interface',
@@ -3382,5 +3490,7 @@ __all__ = [
     'parse_show_tftp_server', 'parse_config_tftp_server_enable',
     'parse_config_tftp_server_no_enable', 'parse_config_tftp_server_path',
     'parse_config_tftp_server_no_path', 'parse_show_interface_lag',
-    'parse_show_mirror'
+    'parse_show_mirror',
+    'parse_config_tftp_server_no_path', 'parse_show_snmp_community',
+    'parse_show_snmp_system', 'parse_show_trap'
 ]
